d1cc6898534ea0741c0a8b6d62deb42c2cb63455
qspiders12345 <39665007+qspiders12345@users.noreply.github.com>
2019-10-19 06:36:31 +0530

    Add files via upload
    Gecko driver key and value commented.

--- src/main/java/generics/AutoConstant.java
+++ src/main/java/generics/AutoConstant.java
@@ -4,8 +4,8 @@ public interface AutoConstant
 {
 	String chrome_key="webdriver.chrome.driver";
 	String chrome_value="E:/Softwares/chromedriver.exe";
-	String gecko_key="webdriver.gecko.driver";
-	String gecko_value="E:/Softwares/geckodriver.exe";
+	/*String gecko_key="webdriver.gecko.driver";
+	String gecko_value="E:/Softwares/geckodriver.exe";*/
 	String ie_key="webdriver.ie.driver";
 	String ie_value="E:/Softwares/IEDriverServer.exe";
 	String excel_path="E:/Selenium147/Practice/FrameworkData.xlsx";
